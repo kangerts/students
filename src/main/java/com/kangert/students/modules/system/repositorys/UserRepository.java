83b78553007614d330be8fdfc1a4926aabc7ae57
kangert <kangert@qq.com>
2021-05-08 11:45:33 +0800

    优化

--- src/main/java/com/kangert/students/modules/system/repositorys/UserRepository.java
+++ src/main/java/com/kangert/students/modules/system/repositorys/UserRepository.java
@@ -2,12 +2,10 @@
  * @Author: kangert
  * @Email: kangert@qq.com
  * @Date: 2021-04-29 20:47:37
- * @LastEditTime: 2021-04-30 09:30:01
+ * @LastEditTime: 2021-05-08 11:45:12
  * @Description: 用户数据仓库
  */
 package com.kangert.students.modules.system.repositorys;
-
-import java.util.List;
 
 import com.kangert.students.modules.system.entitys.UserEntity;
 

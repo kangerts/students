00d376ecf21419405535bb4c773f48eaeabce0b8
kangert <kangert@qq.com>
2021-05-13 11:10:43 +0800

    优化

--- src/main/java/com/kangert/students/handlers/JwtAuthHandler.java
+++ src/main/java/com/kangert/students/handlers/JwtAuthHandler.java
@@ -2,7 +2,7 @@
  * @Author: kangert
  * @Email: kangert@qq.com
  * @Date: 2021-04-27 15:15:54
- * @LastEditTime: 2021-04-30 17:00:34
+ * @LastEditTime: 2021-05-13 11:10:32
  * @Description: JWT验证处理工具类
  */
 package com.kangert.students.handlers;
@@ -38,20 +38,30 @@ public class JwtAuthHandler extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        // 获取jwt
+        /**
+         * 获取jwt，Claims
+         */
         String jwt = request.getHeader(jwtUtil.getHeader());
-        // 判断jwt为空还是未定义
+        Claims claims = jwtUtil.getClaimsByToken(jwt);
+
+        /**
+         * 判断jwt为空还是未定义
+         */
         if (StrUtil.isBlankOrUndefined(jwt)) {
             chain.doFilter(request, response);
             return;
         }
-        Claims claims = jwtUtil.getClaimsByToken(jwt);
-        // 不存在
+
+        /**
+         * 不存在
+         */
         if (claims == null) {
             throw new JwtException("Token异常！");
         }
 
-        // jwt过期
+        /**
+         * jwt过期
+         */
         if (jwtUtil.isTokenExpire(claims)) {
             throw new JwtException("Token已过期");
         }
